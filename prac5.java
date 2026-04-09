cd6f19ffbcecb929b07b5bf83d49ffa3aa43d6c4
Heindrich Jansen <heindrichjansen@gmail.com>
2026-04-09 14:34:55 +0200

    imports

--- prac5.java
+++ prac5.java
@@ -1,3 +1,6 @@
-public class prac5 {
-    
-}
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
